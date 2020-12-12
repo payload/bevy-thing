0847311784533b8289277abad277cc68869cd159
Gilbert Röhrbein <gilbert@ifsr.de>
2020-12-13 00:38:10 +0100

    camera movement through input

---
+++ src/input.rs
@@ -0,0 +1,115 @@
+use crate::stuff;
+
+use bevy::{
+    input::{
+        keyboard::KeyboardInput,
+        mouse::{MouseButtonInput, MouseMotion, MouseWheel},
+    },
+    prelude::*,
+    render::camera::{Camera, OrthographicProjection},
+};
+
+#[derive(Default)]
+pub struct MyInputState {
+    keys: EventReader<KeyboardInput>,
+    cursor: EventReader<CursorMoved>,
+    motion: EventReader<MouseMotion>,
+    mousebtn: EventReader<MouseButtonInput>,
+    scroll: EventReader<MouseWheel>,
+}
+
+macro_rules! debug {
+    ($($arg:tt)*) => ({
+        if false { eprintln!($($arg)*); }
+    })
+}
+
+pub fn my_input_system(
+    mut state: ResMut<MyInputState>,
+    ev_keys: Res<Events<KeyboardInput>>,
+    ev_cursor: Res<Events<CursorMoved>>,
+    ev_motion: Res<Events<MouseMotion>>,
+    ev_mousebtn: Res<Events<MouseButtonInput>>,
+    ev_scroll: Res<Events<MouseWheel>>,
+) {
+    // Keyboard input
+    for ev in state.keys.iter(&ev_keys) {
+        if ev.state.is_pressed() {
+            debug!("Just pressed key: {:?}", ev.key_code);
+        } else {
+            debug!("Just released key: {:?}", ev.key_code);
+        }
+    }
+
+    // Absolute cursor position (in window coordinates)
+    for ev in state.cursor.iter(&ev_cursor) {
+        debug!("Cursor at: {}", ev.position);
+    }
+
+    // Relative mouse motion
+    for ev in state.motion.iter(&ev_motion) {
+        debug!("Mouse moved {} pixels", ev.delta);
+    }
+
+    // Mouse buttons
+    for ev in state.mousebtn.iter(&ev_mousebtn) {
+        if ev.state.is_pressed() {
+            debug!("Just pressed mouse button: {:?}", ev.button);
+        } else {
+            debug!("Just released mouse button: {:?}", ev.button);
+        }
+    }
+
+    // scrolling (mouse wheel, touchpad, etc.)
+    for ev in state.scroll.iter(&ev_scroll) {
+        debug!(
+            "Scrolled vertically by {} and horizontally by {}.",
+            ev.y, ev.x
+        );
+    }
+}
+
+pub fn system(
+    game_state: Res<stuff::GameState>,
+    time: Res<Time>,
+    (keys, btns, windows): (Res<Input<KeyCode>>, Res<Input<MouseButton>>, Res<Windows>),
+    mut transforms: Query<&mut Transform>,
+) {
+    if keys.pressed(KeyCode::Space) {
+        // let mut camera = cameras.get_mut(game_state.camera).expect("camera component");
+
+        let mut trans = transforms
+            .get_mut(game_state.camera)
+            .expect("camera transform");
+
+        trans.scale *= 1.1;
+    }
+
+    let pressed = |code| if keys.pressed(code) { 1.0 } else { 0.0 };
+    let x = pressed(KeyCode::D) - pressed(KeyCode::A);
+    let y = pressed(KeyCode::W) - pressed(KeyCode::S);
+    let s = pressed(KeyCode::Plus) - pressed(KeyCode::Minus);
+    let r = pressed(KeyCode::E) - pressed(KeyCode::Q);
+    if x != 0.0 || y != 0.0 || s != 0.0 || r != 0.0 {
+        let mut transform = transforms
+            .get_mut(game_state.camera)
+            .expect("camera transform");
+
+        let vec = Vec3::new(x, y, 0.0);
+        let scale_power = 1.1_f32.powf(s);
+        let scale = Vec3::new(scale_power, scale_power, 1.0);
+        let trans = 40.0 * vec * time.delta_seconds();
+        let angle = 3.14159 * 0.1 * r * time.delta_seconds();
+
+        transform.scale *= scale;
+        transform.translation += trans * scale;
+        transform.rotate(Quat::from_rotation_z(angle));
+    }
+
+    if btns.just_pressed(MouseButton::Left) {}
+
+    if let Some(window) = windows.get_primary() {
+        let pos = window.cursor_position();
+        debug!("XXX {:?}", pos);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,26 @@
-use bevy::prelude::*;
-use rand::prelude::*;
+#![feature(bool_to_option)]
+
+mod input;
+mod stuff;
+
+use bevy::{
+    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+};
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_startup_system(demo_assets_bit_pack)
-        .add_system(atlas_tinyview_hover)
-        .add_system(cursor_system)
+        .add_startup_system(stuff::setup)
+        .add_startup_system(stuff::demo_assets_bit_pack)
+        .add_system(stuff::atlas_tinyview_hover)
+        .add_system(stuff::cursor_system)
+        .add_resource(input::MyInputState::default())
+        .add_system_to_stage(stage::PRE_UPDATE, input::my_input_system)
+        .add_system(input::system)
+        .add_system(bevy::input::system::exit_on_esc_system)
+        .add_plugin(FrameTimeDiagnosticsPlugin)
+        // .add_system(|diagnostics: Res<Diagnostics>| println!("{:?}", diagnostics.get(FrameTimeDiagnosticsPlugin::FPS).unwrap().average()))
+        // .add_system_to_stage(stage::LAST, |mut ev: ResMut<Events<AppExit>>| ev.send(AppExit))
         .run();
-}
-
-fn setup(commands: &mut Commands) {
-    let camera = commands
-        .spawn(Camera2dBundle::default())
-        .current_entity()
-        .unwrap();
-    commands.insert_resource(MyCursorState {
-        main_camera: camera,
-        world_pos: None,
-    });
-}
-
-fn demo_assets_bit_pack(
-    commands: &mut Commands,
-    asset_server: Res<AssetServer>,
-    mut atlases: ResMut<Assets<TextureAtlas>>,
-) {
-    let texture_handle = asset_server.load("bit-pack/Tilesheet/monochrome_transparent.png");
-    let size = Vec2::new(16.0, 16.0);
-    let padding = Vec2::new(1.0, 1.0);
-    let cols = 768 / 16;
-    let rows = 352 / 16;
-    let atlas = TextureAtlas::from_grid_with_padding(texture_handle, size, cols, rows, padding);
-    let atlas_handle = atlases.add(atlas);
-    let atlas = atlases.get(&atlas_handle).unwrap();
-    let spacing_factor = 1.5;
-
-    for index in 0..atlas.len() {
-        let rect = atlas.textures.get(index).unwrap();
-        let sprite = TextureAtlasSprite {
-            index: index as u32,
-            color: Color::GREEN,
-        };
-        commands.spawn(SpriteSheetBundle {
-            texture_atlas: atlases.get_handle(&atlas_handle),
-            sprite,
-            transform: Transform {
-                translation: Vec3::new(
-                    spacing_factor * (rect.min.x - atlas.size.x * 0.5),
-                    -spacing_factor * (rect.min.y - atlas.size.y * 0.5),
-                    0.0,
-                ),
-                scale: Vec3::splat(1.0),
-                rotation: Quat::default(),
-            },
-            ..Default::default()
-        });
-    }
-}
-
-fn atlas_tinyview_hover(
-    cursor: Res<MyCursorState>,
-    atlantes: Res<Assets<TextureAtlas>>,
-    mut query: Query<(
-        &GlobalTransform,
-        &mut TextureAtlasSprite,
-        &Handle<TextureAtlas>,
-    )>,
-) {
-    if let Some(cursor_pos) = cursor.world_pos {
-        for (transform, mut sprite, atlas_handle) in query.iter_mut() {
-            let atlas = atlantes.get(atlas_handle).expect("get atlas");
-            let rect = atlas
-                .textures
-                .get(sprite.index as usize)
-                .expect("get atlas texture rect");
-            let pos = Vec2::from(transform.translation);
-            let size = Vec2::new(rect.width(), rect.height());
-            let tl_pos = pos - 0.5 * size;
-            let br_pos = pos + 0.5 * size;
-            if tl_pos.x <= cursor_pos.x
-                && tl_pos.y <= cursor_pos.y
-                && br_pos.x >= cursor_pos.x
-                && br_pos.y >= cursor_pos.y
-            {
-                if sprite.color != Color::RED {
-                    sprite.color = Color::RED;
-                }
-            } else if sprite.color != Color::WHITE {
-                sprite.color = Color::WHITE;
-            }
-        }
-    } else {
-        for (_transform, mut sprite, _atlas_handle) in query.iter_mut() {
-            if sprite.color != Color::WHITE {
-                sprite.color = Color::WHITE
-            }
-        }
-    }
-}
-
-struct MyCursorState {
-    main_camera: Entity,
-    world_pos: Option<Vec2>,
-}
-
-fn cursor_system(
-    mut state: ResMut<MyCursorState>,
-    windows: Res<Windows>,
-    q_camera: Query<&Transform>,
-) {
-    if let Some((window, cursor)) = windows.get_primary().and_then(|window| {
-        window
-            .cursor_position()
-            .and_then(|cursor| Some((window, cursor)))
-    }) {
-        let camera_transform = q_camera.get(state.main_camera).unwrap();
-        let window_size = Vec2::new(window.width() as f32, window.height() as f32);
-
-        // the default orthographic projection is in pixels from the center;
-        // just undo the translation
-        let pos = cursor - window_size * 0.5;
-
-        // apply the camera transform
-        let world_pos = camera_transform.compute_matrix() * pos.extend(0.0).extend(1.0);
-        state.world_pos = Some(Vec2::from(world_pos));
-    } else {
-        state.world_pos = None;
-    }
-}
-
-fn random_angle() -> f32 {
-    2.0 * 3.14159 * (-0.5 + rand::random::<f32>())
 }
